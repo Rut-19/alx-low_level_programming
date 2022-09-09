9b9e9182c330b13a79e0bb797ed9530ee21ead7e
Rut-19 <rut.workineh7@gmail.com>
2022-09-09 04:15:03 -0700

    task 4 again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Printing
+ * main - Printing the string in the put function
  *
+ * Description: using the main function
  * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
