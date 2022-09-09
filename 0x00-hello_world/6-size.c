f589231b31939fc2ce797ef1def28cc5c122f645
Rut-19 <rut.workineh7@gmail.com>
2022-09-09 06:55:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,7 @@ int main(void)
 	printf("Size of an int: %1d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %1d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %1d byte(s)\n", sizeof(lli));
-	printf("Size of a float: %1d byte(s)\n", sizeof(f));	
+	printf("Size of a float: %1d byte(s)\n", sizeof(f));
+
 return (0);
 }
